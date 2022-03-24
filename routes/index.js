21bee9b985cb12d90dd4c710e472064ffcc6c69a
Dolzh25 <gidravlika87@yandex.ru>
2022-03-24 15:47:00 +0300

    Добавляет роут выхода

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const userRouters = require('./users');
 const moviesRouters = require('./movies');
-const { createUser, login } = require('../controllers/users');
+const {
+  createUser,
+  login,
+  checkAuth,
+  signOut,
+} = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const { validateCreateUser, validateLogin } = require('../middlewares/validate');
 
@@ -10,6 +15,8 @@ router.use('/signin', validateLogin, login);
 
 router.use(auth);
 
+router.delete('/signout', auth, signOut);
+router.get('/check-auth', auth, checkAuth);
 router.use('/users', auth, userRouters);
 router.use('/movies', auth, moviesRouters);
 
