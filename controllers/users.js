bb521f64ddb7ac5dc832c0cb1b4b73a4a6f7bd7d
Dolzh25 <gidravlika87@yandex.ru>
2022-03-25 15:26:09 +0300

    Изменяет параметры cookie

--- controllers/users.js
+++ controllers/users.js
@@ -50,7 +50,7 @@ const login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 72,
         httpOnly: true,
-        sameSite: 'Lax',
+        sameSite: 'none',
       })
         .send({ message: 'Пользователь авторизован' });
     })
